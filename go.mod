fc56f9fd1a1a9c48c6278adefc449667476de4dd
“suryansh <suryanshv.ug23.ee@nitp.ac.in>
2026-08-16 22:27:58 +0530

    first namespace version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mydocker
 
 go 1.25.3
+
+require golang.org/x/sys v0.47.0
--- mydocker/main.go
+++ mydocker/main.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func main() {
-	cmd := exec.Command("/bin/bash")
+	if len(os.Args) < 3 {
+		fmt.Println("usage: mydocker run <command>")
+		os.Exit(1)
+	}
+
+	if os.Args[1] == "child" {
+		child()
+		return
+	}
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		fmt.Println("unknown command")
+		os.Exit(1)
+	}
+}
+
+func run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: unix.CLONE_NEWPID,
+	}
+	if err := cmd.Run(); err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	fmt.Println("running inside container")
+
+	cmd := exec.Command(os.Args[2])
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
